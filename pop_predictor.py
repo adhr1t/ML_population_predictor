6bd7465e993bd5cf0194b75e7d234e86520a1aa6
adhr1t <adhrit.srivastav@gmail.com>
2021-06-16 09:23:37 -0500

    added to ML code

--- pop_predictor.py
+++ pop_predictor.py
@@ -11,6 +11,17 @@ import warnings
 warnings.filterwarnings("ignore")
 
 df = pd.read_csv("worldPopulations.csv")
+ind = df.loc[df['Country Name']=='India']
 
 #remove Country Code, Indicator Name, and Indicator Code
-df.drop(["Country Code", "Indicator Name", "Indicator Code"], axis=1, inplace=True)
+ind.drop(["Country Name","Country Code", "Indicator Name", "Indicator Code"], axis=1, inplace=True)
+ind = ind.T
+#df.drop(["Country Code", "Indicator Name", "Indicator Code"], axis=1, inplace=True)
+
+#remove rows with None values
+ind.dropna(inplace = True)
+ind.head()
+
+#change column names
+ind.reset_index().rename(columns = {"index":"year", 107:"population"})
+ind.head()
